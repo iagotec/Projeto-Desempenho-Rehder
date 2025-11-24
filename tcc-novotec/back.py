a0ec10355840b967163b0739aa82238174c5a23e
cyber_ils <iagofreefire88@gmail.com>
2025-11-24 06:19:11 -0300

    atualizando

--- tcc-novotec/back.py
+++ tcc-novotec/back.py
@@ -1,36 +1,39 @@
 import sqlite3
-#desempenho semanais
-dbase = sqlite3.connect('registros_semanais.db')
-u= dbase.cursor()
-dbase.execute(''' CREATE TABLE IF NOT EXISTS salas_de_aula(
-                    SALA PRIMARY KEY NOT NULL,
-                    SEMANA_1 INT,   
-                    SEMANA_2 INT, 
-                    SEMANA_3 INT,
-                    SEMANA_4 INT,
-                    MES INT)''')
 
-#aplica as mudanças
+# Conexão com o banco
+dbase = sqlite3.connect('registros_semanais.db')
+u = dbase.cursor()
+
+# Criação da tabela
+u.execute('''CREATE TABLE IF NOT EXISTS salas_de_aula (
+                SALA TEXT PRIMARY KEY NOT NULL,
+                SEMANA_1 INT,
+                SEMANA_2 INT,
+                SEMANA_3 INT,
+                SEMANA_4 INT,
+                MES INT)''')
 dbase.commit()
 
-#escreve dentro da data base
-def adicionar(SALA, SEMANA_1,SEMANA_2,SEMANA_3,SEMANA_4,MES):
+def adicionar(SALA, S1, S2, S3, S4, MES):
     try:
-        
-        u.execute('''INSERT OR REPLACE INTO salas_de_aula(SALA, SEMANA_1, SEMANA_2, SEMANA_3, SEMANA_4, MES) VALUES(?, ?, ?, ?, ?, ?)''',(SALA, SEMANA_1, SEMANA_2, SEMANA_3, SEMANA_4, MES))
+        u.execute(
+            '''INSERT OR REPLACE INTO salas_de_aula 
+               (SALA, SEMANA_1, SEMANA_2, SEMANA_3, SEMANA_4, MES)
+               VALUES (?, ?, ?, ?, ?, ?)''',
+            (SALA, S1, S2, S3, S4, MES)
+        )
         dbase.commit()
     except sqlite3.Error as e:
-        print('erro ao inserir dados ',e)
+        print("Erro ao inserir: ", e)
 
+def listar():
+    u.execute("SELECT * FROM salas_de_aula")
+    return u.fetchall()
 
-def listabox():
-    
-    u.execute('''SELECT *  FROM salas_de_aula''')
-    data = u.fetchall()
-    return data
+def deletar(SALA):
+    u.execute("DELETE FROM salas_de_aula WHERE SALA = ?", (SALA,))
+    dbase.commit()
 
-#dados a sala
 def obter_dados(SALA):
-    u.execute('''SELECT * FROM salas_de_aula WHERE SALA = ?''', (SALA,))
-    data=u.fetchone()
-    return data
+    u.execute("SELECT * FROM salas_de_aula WHERE SALA = ?", (SALA,))
+    return u.fetchone()
